425c2ab82bc57febd2be67ed09ff7351ec1b5aa2
kamal lama <developer09kamallama@gmail.com>
2017-10-19 08:13:14 +0545

    Data Types

---
+++ 30DaysOfCode/python/Day2-Data-Types/answer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+i = 4
+d = 4.0
+s = 'HackerRank'
+a=12
+b=4.0
+c=' is the best place to learn and practice coding!'
+a=int(raw_input('Enter a Integer Value: '))
+b=float(raw_input('Enter a Float/double Value: '))
+c=str(raw_input('Enter a String Value: '))
+print i+a
+print d+b
+print s+c
